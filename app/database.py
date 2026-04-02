8fb9c9c73870bbd35e5ad89d45ddb73e2faf15cc
mdazharulislamnk <mdazharulislamnk@gmail.com>
2026-04-02 22:01:10 +0600

    Switch to SQLite database to fix Render suspension

--- app/database.py
+++ app/database.py
@@ -4,7 +4,8 @@ import os
 # Use SQLite for simplicity initially, but easy to switch to Postgres
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./trading.db")
 
-engine = create_engine(DATABASE_URL, echo=True)
+connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
